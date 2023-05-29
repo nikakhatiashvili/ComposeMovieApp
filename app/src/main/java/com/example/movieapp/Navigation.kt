ef3ff7e7455c91b6419c0e785cc6040e9d4a4dcd
nika khatiashvili <khatiashvilinika44@gmail.com>
2023-05-29 15:18:07 +0400

    added detail screen and navigation

--- app/src/main/java/com/example/movieapp/Navigation.kt
+++ app/src/main/java/com/example/movieapp/Navigation.kt
@@ -1,13 +1,13 @@
 package com.example.movieapp
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavType.Companion.IntType
-import androidx.navigation.NavType.Companion.StringType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -43,6 +43,8 @@ fun DetailScreen(id:Int) {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
-           Text(text = id.toString())
+        Column() {
+            Text(text = id.toString())
+        }
     }
 }
